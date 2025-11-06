2c24b86d3938d0574baa241f532ffe95713c61de
dstiscar <dstiscar@ucsc.edu>
2025-11-06 20:15:00 +0000

    draw leaflet map on screen

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+const mapDiv = document.createElement("div");
+mapDiv.id = "map";
+document.body.append(mapDiv);
+
+const CLASSROOM_LATLNG = leaflet.latLng(
+  36.997936938057016,
+  -122.05703507501151,
+);
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+
+const map = leaflet.map(mapDiv, {
+  center: CLASSROOM_LATLNG,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
